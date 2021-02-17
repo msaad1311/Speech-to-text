4277832b0c6993ca38897434b73620b51ec5fa1f
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-02-17 13:53:21 -0500

    added tester jupyter notebook

--- voice.py
+++ voice.py
@@ -17,10 +17,12 @@
 import speech_recognition as sr
 import pyaudio
 
-init_rec = sr.Recognizer()
-print("Let's speak!!")
-with sr.Microphone() as source:
-    audio_data = init_rec.record(source, duration=1)
-    print("Recognizing your text.............")
-    text = init_rec.recognize_google(audio_data)
-    print(text)
+while True:
+    init_rec = sr.Recognizer()
+    print("Let's speak!!")
+    with sr.Microphone() as source:
+        audio_data = init_rec.record(source, duration=1)
+        # print(audio_data)
+        print("Recognizing your text.............")
+        text = init_rec.recognize_google(audio_data,show_all=True)
+        print(text)
