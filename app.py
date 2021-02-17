6c01e7c0912b41c6661daeaca2844473f441f8b5
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-02-17 16:27:11 -0500

    updated the code

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import speech_recognition as sr
 import pyaudio
 import io
 import soundfile as sf
-import pydub
+from pydub import AudioSegment
 import numpy as np
+import os
 from scipy.io.wavfile import write
 
 app = Flask(__name__)
 
 
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-CHUNK = 1024
+CHANNELS = 1
+RATE = 16000
+CHUNK = int(RATE/20)
 RECORD_SECONDS = 5
 init_rec = sr.Recognizer()
 
@@ -39,41 +40,51 @@ def genHeader(sampleRate, bitsPerSample, channels):
     o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
     return o
 
+def soundplot(stream):
+    data = np.fromstring(stream.read(CHUNK),dtype=np.float32)
+    return data
+
 @app.route('/audio')
 def audio():
     # start Recording
     def sound():
 
-        CHUNK = 1024
-        sampleRate = 44100
-        bitsPerSample = 16
-        channels = 2
-        wav_header = genHeader(sampleRate, bitsPerSample, channels)
+        # CHUNK = 1024
+        # sampleRate = 16000
+        # bitsPerSample = 16
+        # channels = 1
+        # duration = 2
+        # wav_header = genHeader(sampleRate, bitsPerSample, channels)
 
         stream = audio1.open(format=FORMAT, channels=CHANNELS,
                         rate=RATE, input=True,input_device_index=1,
                         frames_per_buffer=CHUNK)
         print("recording...")
-        #frames = []
-        first_run = True
-        while True:
-            print('into the while')
-            if first_run:
-                data = wav_header + stream.read(CHUNK)
-                first_run = False
-            else:
-                data = stream.read(CHUNK)
-            print(type(data))
-            write('tester.wav',16000,np.array(data,dtype=np.int16))
-            with sr.AudioFile('tester.wav') as source:
-                data= init_rec.record(source)
+        frames = []
+        for i in range(0,int(RATE/CHUNK * RECORD_SECONDS)):
+            #if i%10==0: print(i) 
+            print(i)
+            # d=soundplot(stream)
+            frames.append(soundplot(stream)) 
+        data= np.array(frames).reshape(-1)
+        print(type(data))
+        try:
+            print('into the try catch of saving')
+            write('tester.wav',16000,data)
+        except:
+            pass
+        print('starting to reuse')
+        with sr.AudioFile('tester.wav') as source:
+            print('into the reusable')
+            data= init_rec.record(source)
             try:
                 text = init_rec.recognize_google(data)
                 print(text)
                 yield(text)
             except Exception as e:
                 print("Exception: "+str(e))
-            
+        os.remove('tester.wav')
+        
 
     return Response(sound())
 
