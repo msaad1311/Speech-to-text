f387cfd41e7a7738a915716be36f77b9f3c2f7e4
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-02-17 14:03:03 -0500

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,19 +1,3 @@
-# import speech_recognition as sr
-# import time
-
-# while True:
-#     r=sr.Recognizer()
-#     with sr.Microphone() as source:
-#         print("Listening!")
-#         stime = time.time()
-#         audio=r.listen(source)
-#         print(time.time()-stime)
-#         try:
-#             print('into the try method')
-#             s_name=r.recognize_google(audio)
-#             print(s_name)
-#         except:
-#             print('error')
 import speech_recognition as sr
 import pyaudio
 
@@ -24,5 +8,8 @@ while True:
         audio_data = init_rec.listen(source)
         # print(audio_data)
         print("Recognizing your text.............")
-        text = init_rec.recognize_google(audio_data)
-        print(text)
+        try:
+            text = init_rec.recognize_google(audio_data)
+            print(text)
+        except:
+            print('Error while reading the file')
