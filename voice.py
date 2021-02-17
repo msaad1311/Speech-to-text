abd9d8cb8269aff7ea405d9240fc59ea85d4d43f
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-02-17 11:50:11 -0500

    added 2 files

--- voice.py
+++ voice.py
@@ -1,16 +1,26 @@
-import speech_recognition as sr
-import time
+# import speech_recognition as sr
+# import time
 
-while True:
-    r=sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening!")
-        stime = time.time()
-        audio=r.listen(source,phrase_time_limit=1)
-        print(time.time()-stime)
-        try:
-            print('into the try method')
-            s_name=r.recognize_google(audio)
-            print(s_name)
-        except:
-            print("Error!")
+# while True:
+#     r=sr.Recognizer()
+#     with sr.Microphone() as source:
+#         print("Listening!")
+#         stime = time.time()
+#         audio=r.listen(source)
+#         print(time.time()-stime)
+#         try:
+#             print('into the try method')
+#             s_name=r.recognize_google(audio)
+#             print(s_name)
+#         except:
+#             print('error')
+import speech_recognition as sr
+import pyaudio
+
+init_rec = sr.Recognizer()
+print("Let's speak!!")
+with sr.Microphone() as source:
+    audio_data = init_rec.record(source, duration=1)
+    print("Recognizing your text.............")
+    text = init_rec.recognize_google(audio_data)
+    print(text)
